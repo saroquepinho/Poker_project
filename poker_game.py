41d18c9763ce577a0ca5a8b267a6f2c191d2af58
saroquepinho <salvadorkrp@gmail.com>
2025-03-19 18:51:22 +0100

    Poker game updated first version of poker_game.py

--- poker_game.py
+++ poker_game.py
@@ -16,8 +16,23 @@ class PokerHand:
     def __str__(self):
         return str(self.cards)
 
-deck = Deck()
-deck.shuffle()
-hand = PokerHand(deck)
-print(hand)
+    @property
+    def is_flush(self):
+        for card in self.cards[1:]:
+            if self.cards[0].suit != card.suit:
+                return False
+        return True
+
+
+count = 0
+flushes = 0
+while flushes < 100:
+    deck = Deck()
+    deck.shuffle()
+    hand = PokerHand(deck)
+    if hand.is_flush:
+        flushes += 1
+    count += 1
+
+print(f"probability of a flush is {100*flushes/count}%")
 
